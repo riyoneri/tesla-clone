770ce5481f5b00c43d7db96e175c4f269662bbcf
Lionel <lionkaneza@gmail.com>
2023-06-10 12:39:18 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        main: "",
+        "main-light": "",
+        "main-bold": ""
+      },
+      backgroundImage: {
+        
+      },
+      colors: {
+        "main": "",
+        "hover": "",
+      }
+    },
+  },
+  plugins: [],
+}
+
