045a71d609968579010ce0bffd213ce09a306eff
riyoneri <lionkaneza@gmail.com>
2023-06-15 08:58:33 +0200

    Add more sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,17 @@ module.exports = {
         "main-medium": "Gotham-Medium"
       },
       backgroundImage: {
-        
+        "top-ad": "radial-gradient(54.97% 272.54% at 27.36% -134.72%,rgba(108,148,255,.92) 0,rgba(0,0,0,.92) 100%)",
+        "models": "url('/assets/images/models.jpeg')",
+        "blue-car": "url('/assets/images/blue car.avif')"
       },
       colors: {
-        "main": "",
-        "hover": "",
+        "primary": "#171a20",
+        "light-dark": "rgba(0, 0, 0, 0.15)",
+        "graysh-light": "#e2e3e3"
+      },
+      boxShadow: {
+        "bottom-lg": "box-shadow: inset 0 0 500px 8px #000"
       }
     },
   },
